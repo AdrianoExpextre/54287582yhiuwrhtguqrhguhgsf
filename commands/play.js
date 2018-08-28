1f7edb652d0626e70c19cad9677c73ab0233b578
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-08-28 02:55:46 -0300

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -12,6 +12,16 @@ var fetchVideoInfo = require('youtube-info');
 
 exports.run = async(music, message, args, queue) => {
   
+  let db = require('../database.js');
+  db.Bloqueio.findOne({"_id": message.author.id}, function (erro, documento) {
+                if(documento) {
+         if (!['244489368717230090'].includes(message.author.id))
+                
+ if ([documento.block].includes(message.author.id)) return message.channel.send(`<:xguardian:476061993368027148> ${message.author}, você foi bloqueado de usar comandos do **Sysop**, se você acha que isso é um engano nos contate!`);
+        
+}
+})
+  
   if (!['244489368717230090'].includes(message.author.id)) return message.channel.send(`<:sysalerta:469789950938841088> Opa ${message.author}, comando em manutenção!`);
 
   
@@ -306,7 +316,7 @@ const embed4 = new Discord.RichEmbed()
             }
           })
 }
-
+});
 } 
 
 
